1f97c8a7c13cf0141c1ac9a216b296fc31240857
Greg Cotten <gregcotten@gmail.com>
2012-04-24 02:36:32 -0700

    changed ownership header

--- MocoJo/libraries/MocoJo/MocoTimer1.cpp
+++ MocoJo/libraries/MocoJo/MocoTimer1.cpp
@@ -1,31 +1,6 @@
 /*
-  MsTimer2.h - Using timer2 with 1ms resolution
-  Javier Valencia <javiervalencia80@gmail.com>
-  
-  History:
-  	29/May/09 - V0.5 added support for Atmega1280 (thanks to Manuel Negri)
-  	19/Mar/09 - V0.4 added support for ATmega328P (thanks to Jerome Despatis)
-  	11/Jun/08 - V0.3 
-  		changes to allow working with different CPU frequencies
-  		added support for ATMega128 (using timer2)
-  		compatible with ATMega48/88/168/8
-	10/May/08 - V0.2 added some security tests and volatile keywords
-	9/May/08 - V0.1 released working on ATMEGA168 only
-	
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  MocoTimer1.h - Using timer1 with 1ms resolution
+  Greg Cotten
 */
 
 #include <MocoTimer1.h>
