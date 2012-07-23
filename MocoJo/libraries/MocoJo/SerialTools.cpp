b4ec2ce54ef1090addc78ccfa4ca5f1278b3d292
Greg Cotten <gregcotten@gmail.com>
2012-07-22 22:26:23 -0700

    added writing and reading of ints

--- MocoJo/libraries/MocoJo/SerialTools.cpp
+++ MocoJo/libraries/MocoJo/SerialTools.cpp
@@ -2,16 +2,25 @@
 #include <WProgram.h>
 #include <WString.h>
 
-void SerialTools::writeLongToSerial(long number)
-{
+void SerialTools::writeIntToSerial(int number){
+    Serial.write((uint8_t)((number >> 8) & 0XFF));
+	Serial.write((uint8_t)((number & 0XFF)));
+}
+
+int SerialTools::readIntFromSerial(){
+    byte byte1 = Serial.read();
+    byte byte2 = Serial.read();
+    return (byte1 << 8) + (byte2);
+}
+
+void SerialTools::writeLongToSerial(long number){
 	Serial.write((uint8_t)((number >> 24) & 0xFF));
 	Serial.write((uint8_t)((number >> 16) & 0xFF));
 	Serial.write((uint8_t)((number >> 8) & 0XFF));
 	Serial.write((uint8_t)((number & 0XFF)));
 }
 
-long SerialTools::readLongFromSerial()
-{
+long SerialTools::readLongFromSerial(){
 	byte byte1 = Serial.read();
 	byte byte2 = Serial.read();
 	byte byte3 = Serial.read();
--- MocoJo/libraries/MocoJo/SerialTools.h
+++ MocoJo/libraries/MocoJo/SerialTools.h
@@ -5,6 +5,8 @@
 #include <WString.h>
 
 namespace SerialTools {
+	void writeIntToSerial(int number);    
+    int readIntFromSerial();
 	void writeLongToSerial(long number);
 	long readLongFromSerial();
 }
