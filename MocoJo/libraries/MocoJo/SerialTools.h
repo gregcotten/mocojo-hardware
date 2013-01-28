f92f4c812c755c714ae64307a1a79b9de17ef929
Greg Cotten <gregcotten@gmail.com>
2013-01-27 23:19:44 -0800

    SMC fixes

--- MocoJo/libraries/MocoJo/SerialTools.h
+++ MocoJo/libraries/MocoJo/SerialTools.h
@@ -5,12 +5,13 @@
 #include <WString.h>
 
 namespace SerialTools {
-	void writeShortToSerial(HardwareSerial stream, int number);    
-    int readShortFromSerial(HardwareSerial stream);
-	void writeLongToSerial(HardwareSerial stream, long number);
-	long readLongFromSerial(HardwareSerial stream);
-	void writeDummyBytesToSerial(HardwareSerial stream, int numberOfBytes);
-	void readDummyBytesFromSerial(HardwareSerial stream, int numberOfBytes);
+	void writeShortToSerial(HardwareSerial serial, int number);    
+    int readShortFromSerial(HardwareSerial serial);
+	void writeLongToSerial(HardwareSerial serial, long number);
+	long readLongFromSerial(HardwareSerial serial);
+	void writeDummyBytesToSerial(HardwareSerial serial, int numberOfBytes);
+	void readDummyBytesFromSerial(HardwareSerial serial, int numberOfBytes);
+	void blockUntilBytesArrive(HardwareSerial serial, int numberOfBytes);
 }
 
 #endif
