a1c1d7d07d37fa7e1963aa3c9c57235d0a22f7c5
Greg Cotten <gregcotten@gmail.com>
2013-01-28 23:00:47 -0800

    SerialTools works correctly now?

--- MocoJo/libraries/MocoJo/SerialTools.cpp
+++ MocoJo/libraries/MocoJo/SerialTools.cpp
@@ -4,46 +4,46 @@
 
 //All send/receive methods use MSB byte order
 
-void SerialTools::writeShortToSerial(HardwareSerial serial, int number){
-    serial.write((uint8_t)((number >> 8) & 0XFF));
-	serial.write((uint8_t)((number & 0XFF)));
+void SerialTools::writeShortToSerial(HardwareSerial &serial, int number){
+    (&serial) -> write((uint8_t)((number >> 8) & 0XFF));
+	(&serial) -> write((uint8_t)((number & 0XFF)));
 }
 
-int SerialTools::readShortFromSerial(HardwareSerial serial){
-    byte byte1 = serial.read();
-    byte byte2 = serial.read();
+int SerialTools::readShortFromSerial(HardwareSerial &serial){
+    byte byte1 = (&serial) -> read();
+    byte byte2 = (&serial) -> read();
     return (byte1 << 8) + (byte2);
 }
 
-void SerialTools::writeLongToSerial(HardwareSerial serial, long number){
-	serial.write((uint8_t)((number >> 24) & 0xFF));
-	serial.write((uint8_t)((number >> 16) & 0xFF));
-	serial.write((uint8_t)((number >> 8) & 0XFF));
-	serial.write((uint8_t)((number & 0XFF)));
+void SerialTools::writeLongToSerial(HardwareSerial &serial, long number){
+	(&serial) -> write((uint8_t)((number >> 24) & 0xFF));
+	(&serial) -> write((uint8_t)((number >> 16) & 0xFF));
+	(&serial) -> write((uint8_t)((number >> 8) & 0XFF));
+	(&serial) -> write((uint8_t)((number & 0XFF)));
 }
 
-long SerialTools::readLongFromSerial(HardwareSerial serial){
-	byte byte1 = serial.read();
-	byte byte2 = serial.read();
-	byte byte3 = serial.read();
-	byte byte4 = serial.read();
+long SerialTools::readLongFromSerial(HardwareSerial &serial){
+	byte byte1 = (&serial) -> read();
+	byte byte2 = (&serial) -> read();
+	byte byte3 = (&serial) -> read();
+	byte byte4 = (&serial) -> read();
 	
 	return ((byte1 << 24) + (byte2 << 16) + (byte3 << 8) + (byte4));
 }
 
-void SerialTools::writeDummyBytesToSerial(HardwareSerial serial, int numberOfDummyBytes){
+void SerialTools::writeDummyBytesToSerial(HardwareSerial &serial, int numberOfDummyBytes){
 	for (int i = 0; i < numberOfDummyBytes; i++){
-		serial.write((uint8_t)1);
+		(&serial) -> write((uint8_t)1);
 	}
 }
 
-void SerialTools::readDummyBytesFromSerial(HardwareSerial serial, int numberOfDummyBytes){
+void SerialTools::readDummyBytesFromSerial(HardwareSerial &serial, int numberOfDummyBytes){
 	for (int i = 0; i < numberOfDummyBytes; i++){
-		serial.read();
+		(&serial) -> read();
 	}
 }
 
-void SerialTools::blockUntilBytesArrive(HardwareSerial serial, int numberOfBytes){
-	while(serial.available() < numberOfBytes){}
+void SerialTools::blockUntilBytesArrive(HardwareSerial &serial, int numberOfBytes){
+	while( (&serial)->available() <= numberOfBytes){}
 }
 
--- MocoJo/libraries/MocoJo/SerialTools.h
+++ MocoJo/libraries/MocoJo/SerialTools.h
@@ -4,13 +4,13 @@
 #include <WProgram.h>
 
 namespace SerialTools {
-	void writeShortToSerial(HardwareSerial serial, int number);    
-    int readShortFromSerial(HardwareSerial serial);
-	void writeLongToSerial(HardwareSerial serial, long number);
-	long readLongFromSerial(HardwareSerial serial);
-	void writeDummyBytesToSerial(HardwareSerial serial, int numberOfBytes);
-	void readDummyBytesFromSerial(HardwareSerial serial, int numberOfBytes);
-	void blockUntilBytesArrive(HardwareSerial serial, int numberOfBytes);
+	void writeShortToSerial(HardwareSerial &serial, int number);    
+    int readShortFromSerial(HardwareSerial &serial);
+	void writeLongToSerial(HardwareSerial &serial, long number);
+	long readLongFromSerial(HardwareSerial &serial);
+	void writeDummyBytesToSerial(HardwareSerial &serial, int numberOfBytes);
+	void readDummyBytesFromSerial(HardwareSerial &serial, int numberOfBytes);
+	void blockUntilBytesArrive(HardwareSerial &serial, int numberOfBytes);
 }
 
 #endif
