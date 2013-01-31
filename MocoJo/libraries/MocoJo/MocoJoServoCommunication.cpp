48e096c948df720b2a1dd1ad2864ef99303f9e9f
Greg Cotten <gregcotten@gmail.com>
2013-01-30 19:47:11 -0800

    servo comm WIP

--- MocoJo/libraries/MocoJo/MocoJoServoCommunication.cpp
+++ MocoJo/libraries/MocoJo/MocoJoServoCommunication.cpp
@@ -2,6 +2,7 @@
 #include <MocoJoServoProtocol.h>
 #include <SerialTools.h>
 
+//SERVO to MCU
 void MocoJoServoCommunication::writeHandshakeSuccessToMCU(HardwareSerial &serial, int ID){
 	(&serial) -> write(ID);
 	(&serial) -> write(MocoJoServoHandshakeSuccessfulResponse);
@@ -25,3 +26,58 @@ void MocoJoServoCommunication::writeMocoJoServoDidHoneToFirstPosition(HardwareSe
 	(&serial) -> write(MocoJoServoDidHoneToFirstPosition);
 	SerialTools::writeDummyBytesToSerial(serial, 4);
 }
+//----------------------
+
+//MCU to SERVO
+
+//init
+void MocoJoServoCommunication::writeHandshakeRequestToServo(HardwareSerial &serial, int ID){
+	(&serial) -> write(ID);
+	(&serial) -> write(MocoJoServoHandshakeRequest);
+	SerialTools::writeDummyBytesToSerial(serial, 4);
+}
+
+void MocoJoServoCommunication::writeExitSafeStartToServo(HardwareSerial &serial, int ID){
+	(&serial) -> write(ID);
+	(&serial) -> write(MocoJoServoExitSafeStart);
+	SerialTools::writeDummyBytesToSerial(serial, 4);
+}
+
+
+
+//limits
+void MocoJoServoCommunication::writeSetMaxSpeedToServo(HardwareSerial &serial, int ID, long maxSpeed){
+	(&serial) -> write(ID);
+	(&serial) -> write(MocoJoServoSetMaxSpeed);
+	SerialTools::writeLongToSerial(serial, maxSpeed);
+}
+
+
+//position
+void MocoJoServoCommunication::writeSetTargetPositionToServo(HardwareSerial &serial, int ID, long targetPosition){
+	(&serial) -> write(ID);
+	(&serial) -> write(MocoJoServoSetTargetPosition);
+	SerialTools::writeLongToSerial(serial, targetPosition);
+}
+
+void MocoJoServoCommunication::writeAddTargetPositionToBufferToServo(HardwareSerial &serial, int ID, long targetPosition){
+	(&serial) -> write(ID);
+	(&serial) -> write(MocoJoServoAddTargetPositionToBuffer);
+	SerialTools::writeLongToSerial(serial, targetPosition);
+}
+
+//playback
+void MocoJoServoCommunication::writeStartPlaybackToServo(HardwareSerial &serial, int ID, long firstPosition){
+	(&serial) -> write(ID);
+	(&serial) -> write(MocoJoServoStartPlayback);
+	SerialTools::writeLongToSerial(serial, targetPosition);
+}
+
+void MocoJoServoCommunication::writeStopPlaybackToServo(HardwareSerial &serial, int ID){
+	(&serial) -> write(ID);
+	(&serial) -> write(MocoJoServoStopPlayback);
+	SerialTools::writeDummyBytesToSerial(serial, 4);
+}
+//-----------------------
+
+
--- MocoJo/libraries/MocoJo/MocoJoServoCommunication.h
+++ MocoJo/libraries/MocoJo/MocoJoServoCommunication.h
@@ -4,12 +4,28 @@
 #include <WProgram.h>
 
 namespace MocoJoServoCommunication {
+	//Servo to MCU
 	void writeHandshakeSuccessToMCU(HardwareSerial &serial, int ID);
 	
 	void writeCurrentPositionToMCU(HardwareSerial &serial, int ID, long currentPosition);
 	void writePositionAtLastSyncToMCU(HardwareSerial &serial, int ID, long positionAtLastSync);
 
 	void writeMocoJoServoDidHoneToFirstPosition(HardwareSerial &serial, int ID);
+	//--------------
+
+
+	//MCU to Servo
+	void writeHandshakeRequestToServo(HardwareSerial &serial, int ID);
+	void MocoJoServoCommunication::writeExitSafeStartToServo(HardwareSerial &serial, int ID);
+	//limits
+	void MocoJoServoCommunication::writeSetMaxSpeedToServo(HardwareSerial &serial, int ID, long maxSpeed);
+	//position
+	void MocoJoServoCommunication::writeSetTargetPositionToServo(HardwareSerial &serial, int ID, long targetPosition);
+	void MocoJoServoCommunication::writeAddTargetPositionToBufferToServo(HardwareSerial &serial, int ID, long targetPosition);
+	//playback
+	void MocoJoServoCommunication::writeStartPlaybackToServo(HardwareSerial &serial, int ID, long firstPosition);
+	void MocoJoServoCommunication::writeStopPlaybackToServo(HardwareSerial &serial, int ID);
+	//--------------
 }
 
 #endif
